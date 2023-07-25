864a87982bab531a5e4d76e986e3eed92770965b
TrggTin <100864189+TrggTin@users.noreply.github.com>
2023-07-25 20:20:53 +0700

    Add files via upload

---
+++ src/database/Book/Book-30-35.sql
@@ -0,0 +1,38 @@
+﻿SET DATEFORMAT DMY;
+USE HappiNovel
+
+INSERT INTO BOOK VALUES('30',N'Nữ Thần Lớp Tôi Có Gì Đó Lạ Lắm.','30'
+,'note30.docx'
+,'summary30.docx'
+,'25-07-2023','coverImg30.png','1','25072003')
+--Book1 của user 30 
+
+INSERT INTO BOOK VALUES('31',N'Khi bắt đầu làm công việc nhà bán thời gian, tôi được gia đình của Idol trường học để ý lúc nào không hay','30'
+,'note31.docx'
+,'summary31.docx'
+,'25-07-2023','coverImg31.png','2','20250')
+--Book2 của user 30 
+
+INSERT INTO BOOK VALUES('32',N'Làm bạn với cô gái đáng yêu thứ hai lớp','34'
+,'note32.docx'
+,'summary32.docx'
+,'25-07-2023','coverImg32.png','0','29250')
+--Book3 của user 34 
+
+INSERT INTO BOOK VALUES('33',N'"Cậu không thể hôn được, phải chứ?" Khi tôi khiến cô bạn thuở nhỏ luôn trêu chọc mình hiểu chuyện, cô ấy đột nhiên trở nên dễ thương hơn nhiều','35'
+,'note33.docx'
+,'summary33.docx'
+,'25-07-2023','coverImg33.png','0','1250')
+--Book4 của user 35 
+
+INSERT INTO BOOK VALUES('34',N'Tôi là Tác giả, phải không?','36'
+,'note34.docx'
+,'summary34.docx'
+,'25-07-2023','coverImg34.png','0','1250')
+--Book5 của user 36 
+
+INSERT INTO BOOK VALUES('35',N'Bức Thư Tình Đến Từ Tương Lai','36'
+,'note35.docx'
+,'summary35.docx'
+,'25-07-2023','coverImg35.png','0','1250')
+--Book6 của user 36 
