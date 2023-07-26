896d2f0a7fe682502f74e6a06215902ad5e5e25f
Albert de Nafta <101512551+AlbertNafta@users.noreply.github.com>
2023-07-26 12:14:25 +0700

    Add files via upload

--- src/database/noti-01-09.sql
+++ src/database/noti-01-09.sql
@@ -14,11 +14,9 @@ INSERT INTO NOTIFY VALUES('1','1','Noti1.txt')
 
 INSERT INTO NOTIFY VALUES('2','2','Noti2.txt')
 --dòng 1: id sách
---dòng 2: id vol
 
 INSERT INTO NOTIFY VALUES('3','3','Noti3.txt')
 --dòng 1: id sách
---dòng 2: id vol
 
 INSERT INTO NOTIFY VALUES('4','4','Noti4.txt')
 --dòng 1: id người dùng
@@ -28,7 +26,7 @@ INSERT INTO NOTIFY VALUES('5','5','Noti5.txt')
 --dòng 1: id sách
 --không cần id người dùng bởi khi ấn vào đó, nó sẽ direct mình đến peding list
 
-INSERT INTO NOTIFY VALUES('6','5','Noti6.txt')
+INSERT INTO NOTIFY VALUES('6','2','Noti6.txt')
 INSERT INTO NOTIFY VALUES('7','4','Noti7.txt')
 INSERT INTO NOTIFY VALUES('8','3','Noti8.txt')
 INSERT INTO NOTIFY VALUES('9','2','Noti9.txt')
