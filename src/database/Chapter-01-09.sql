f38cff81d94444f2b9764c9ce439685d48d08568
Albert de Nafta <101512551+AlbertNafta@users.noreply.github.com>
2023-07-26 18:16:35 +0700

    Add files via upload

--- src/database/Chapter-01-09.sql
+++ src/database/Chapter-01-09.sql
@@ -2,66 +2,71 @@
 SET DATEFORMAT DMY;
 
 INSERT INTO CHAPTER VALUES 
-('1','1','1',N'Chọn bạn mà chơi.','21-07-2023','chap1_1_1.docx')
+('1','1','1',N'Chọn bạn mà chơi.','21-07-2023','chap1_1_1.docx','0')
 
 INSERT INTO CHAPTER VALUES 
-('1','1','2',N'Đi đâu.','21-07-2023','chap2_1_1.docx')
+('1','1','2',N'Đi đâu.','21-07-2023','chap2_1_1.docx','0')
 
 INSERT INTO CHAPTER VALUES 
-('1','2','1',N'Ngủ.','21-07-2023','chap1_2_1.docx')
+('1','2','1',N'Ngủ.','21-07-2023','chap1_2_1.docx','0')
 
 INSERT INTO CHAPTER VALUES 
-('2','1','1',N'Tui luôn muốn.','21-07-2023','chap1_1_2.docx')
+('2','1','1',N'Tui luôn muốn.','21-07-2023','chap1_1_2.docx','0')
 
 INSERT INTO CHAPTER VALUES 
-('2','1','2',N'Chọn tôi mà chơi.','21-07-2023','chap2_1_2.docx')
+('2','1','2',N'Chọn tôi mà chơi.','21-07-2023','chap2_1_2.docx','0')
 
 INSERT INTO CHAPTER VALUES 
-('2','2','1',N'Chọn uiuiybạn mà chơi.','21-07-2023','chap2_2_1.docx')
+('2','2','1',N'Chọn uiuiybạn mà chơi.','21-07-2023','chap2_2_1.docx','0')
 
 INSERT INTO CHAPTER VALUES 
-('2','2','2',N'Cuhọn bạn mà chơi.','21-07-2023','chap2_2_2.docx')
+('2','2','2',N'Cuhọn bạn mà chơi.','21-07-2023','chap2_2_2.docx','0')
 
 INSERT INTO CHAPTER VALUES 
-('3','1','1',N'Chọniui bạn mà chơi.','21-07-2023','chap1_1_3.docx')
+('3','1','1',N'Chọniui bạn mà chơi.','21-07-2023','chap1_1_3.docx','0')
 INSERT INTO CHAPTER VALUES 
-('3','1','2',N'Ngu','21-07-2023','chap2_1_3.docx')
+('3','1','2',N'Ngu','21-07-2023','chap2_1_3.docx','0')
 INSERT INTO CHAPTER VALUES 
-('3','2','1',N'Ăn ăn ăn','21-07-2023','chap1_2_3.docx')
+('3','2','1',N'Ăn ăn ăn','21-07-2023','chap1_2_3.docx','0')
 INSERT INTO CHAPTER VALUES 
-('3','2','2',N'Chó.','21-07-2023','chap2_2_3.docx')
+('3','2','2',N'Chó.','21-07-2023','chap2_2_3.docx','0')
 INSERT INTO CHAPTER VALUES 
-('3','2','3',N'Cô ây','21-07-2023','chap3_2_3.docx')
+('3','2','3',N'Cô ây','21-07-2023','chap3_2_3.docx','0')
 INSERT INTO CHAPTER VALUES 
-('4','1','1',N'Ngô Ngọc Liên','21-07-2023','chap1_1_4.docx')
+('4','1','1',N'Ngô Ngọc Liên','21-07-2023','chap1_1_4.docx','0')
 INSERT INTO CHAPTER VALUES 
-('4','1','2',N'What','21-07-2023','chap2_1_4.docx')
+('4','1','2',N'What','21-07-2023','chap2_1_4.docx','0')
 INSERT INTO CHAPTER VALUES 
-('4','1','3',N'Tôi thích cô ấy','21-07-2023','chap3_1_4.docx')
+('4','1','3',N'Tôi thích cô ấy','21-07-2023','chap3_1_4.docx','0')
 INSERT INTO CHAPTER VALUES 
-('4','2','1',N'Ngô Ngọc Liên','21-07-2023','chap1_2_4.docx')
+('4','2','1',N'Ngô Ngọc Liên','21-07-2023','chap1_2_4.docx','0')
 INSERT INTO CHAPTER VALUES 
-('4','2','2',N'Ngô Ngọc Liên','21-07-2023','chap2_2_4.docx')
+('4','2','2',N'Ngô Ngọc Liên','21-07-2023','chap2_2_4.docx','0')
 
 INSERT INTO CHAPTER VALUES 
-('5','1','1',N'Gì zậy chời.','21-07-2023','chap1_1_5.docx')
+('5','1','1',N'Gì zậy chời.','21-07-2023','chap1_1_5.docx','0')
 INSERT INTO CHAPTER VALUES 
-('5','1','2',N'Ngủ ngủ ngủ.','21-07-2023','chap2_1_5.docx')
+('5','1','2',N'Ngủ ngủ ngủ.','21-07-2023','chap2_1_5.docx','0')
 INSERT INTO CHAPTER VALUES 
-('5','1','3',N'Chọn bạn mà chơi.','21-07-2023','chap3_1_5.docx')
+('5','1','3',N'Chọn bạn mà chơi.','21-07-2023','chap3_1_5.docx','0')
 INSERT INTO CHAPTER VALUES 
-('5','2','1',N'Lại ngủ.','21-07-2023','chap1_2_5.docx')
+('5','2','1',N'Lại ngủ.','21-07-2023','chap1_2_5.docx','0')
 INSERT INTO CHAPTER VALUES 
-('5','2','2',N'Hít hít le le','21-07-2023','chap2_2_5.docx')
+('5','2','2',N'Hít hít le le','21-07-2023','chap2_2_5.docx','0')
 
 INSERT INTO CHAPTER VALUES 
-('6','1','1',N'BÌm bịp.','21-07-2023','chap1_1_6.docx')
+('6','1','1',N'BÌm bịp.','21-07-2023','chap1_1_6.docx','0')
 INSERT INTO CHAPTER VALUES 
-('6','1','2',N'Ngô Ngọc Liên','21-07-2023','chap2_1_6.docx')
+('6','1','2',N'Ngô Ngọc Liên','21-07-2023','chap2_1_6.docx','0')
 INSERT INTO CHAPTER VALUES 
-('6','2','1',N'The hell stop','21-07-2023','chap1_2_6.docx')
+('6','2','1',N'The hell stop','21-07-2023','chap1_2_6.docx','0')
 INSERT INTO CHAPTER VALUES 
-('6','2','2',N'Ngáo xịt tôm','21-07-2023','chap2_2_6.docx')
+('6','2','2',N'Ngáo xịt tôm','21-07-2023','chap2_2_6.docx','0')
 
+INSERT INTO CHAPTER VALUES 
+('8','1','1',N'Le le điên','21-07-2023','chap1_1_8.docx','1')
+INSERT INTO CHAPTER VALUES 
+('8','1','2',N'Nhức đầu vcl','21-07-2023','chap2_1_8.docx','1')
+--hia chap chời duyệt
 
 
