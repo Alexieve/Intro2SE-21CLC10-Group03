2f75ad1011082cbd6c4bf31d230d3a949488c9c4
thinhcu67si <94957063+thinhcu67si@users.noreply.github.com>
2023-07-26 08:11:15 +0700

    Update Book_20-29.sql

--- src/database/Book_20-29.sql
+++ src/database/Book_20-29.sql
@@ -1,13 +1,13 @@
+SET DATEFORMAT DMY;
 INSERT INTO BOOK (BOOKID, TITLE, AUTHOR, NOTE, SUMMARY, PUBLISHDATE, COVERIMAGE, BOOKSTATUS, TOTALVIEW)
 VALUES
-    ('20', N'Đệ Đệ Ta Là Thiên Tuyển Chi Tử', '22', 'note20.docx', 'summary20.docx', '2023-06-20', 'coverImg20.jpg', 1, 12000),
-    ('21', N'Ta Có Một Thân Bị Động Kỹ', '22', 'note21.docx', 'summary21.docx', '2023-06-20', 'coverImg21.jpg', 1, 12000),
-    ('22', N'Cao Võ: Ta Mang Theo Anime Hệ Thống Giết Điên Rồi', '22', 'note22.docx', 'summary22.docx', '2023-06-20', 'coverImg22.jpg', 1, 12000),
-    ('23', N'Ta Trù Thần, Tông Môn Trên Dưới Bị Thèm Khóc Rồi', '23', 'note23.docx', 'summary23.docx', '2023-06-20', 'coverImg23.jpg', 1, 12000),
-    ('24', N'Đan Đạo Tông Sư', '23', 'note24.docx', 'summary24.docx', '2023-06-20', 'coverImg24.jpg', 1, 12000),
-    ('25', N'Ta Ở Group Chat Mô Phỏng Trường Sinh Lộ', '24', 'note25.docx', 'summary25.docx', '2023-06-20', 'coverImg25.jpg', 1, 12000),
-    ('26', N'Trường Sinh Từ Thợ Săn Bắt Đầu', '24', 'note26.docx', 'summary26.docx', '2023-06-20', 'coverImg26.jpg', 1, 12000),
-    ('27', N'Đầu Tư Tộc Nhân,Thực Lực Của Ta Là Tộc Tổng Cộng', '24', 'note27.docx', 'summary27.docx', '2023-06-20', 'coverImg27.jpg', 1, 12000),
-    ('28', N'Toàn Dân Thức Tỉnh:Bắt Đầu Thần Thoại Thiên Phú', '24', 'note28.docx', 'summary28.docx', '2023-06-20', 'coverImg28.jpg', 1, 12000),
-    ('29', N'Bán Tiên', '24', 'note29.docx', 'summary29.docx', '2023-06-20', 'coverImg29.jpg', 1, 12000);
-	
+    ('20', N'Đệ Đệ Ta Là Thiên Tuyển Chi Tử', '22', 'note20.docx', 'summary20.docx', '20-06-2023', 'coverImg20.jpg', 1, 12000),
+    ('21', N'Ta Có Một Thân Bị Động Kỹ', '22', 'note21.docx', 'summary21.docx', '20-06-2023', 'coverImg21.jpg', 1, 12000),
+    ('22', N'Cao Võ: Ta Mang Theo Anime Hệ Thống Giết Điên Rồi', '22', 'note22.docx', 'summary22.docx', '20-06-2023', 'coverImg22.jpg', 1, 12000),
+    ('23', N'Ta Trù Thần, Tông Môn Trên Dưới Bị Thèm Khóc Rồi', '23', 'note23.docx', 'summary23.docx', '20-06-2023', 'coverImg23.jpg', 1, 12000),
+    ('24', N'Đan Đạo Tông Sư', '23', 'note24.docx', 'summary24.docx', '20-06-2023', 'coverImg24.jpg', 1, 12000),
+    ('25', N'Ta Ở Group Chat Mô Phỏng Trường Sinh Lộ', '24', 'note25.docx', 'summary25.docx', '20-06-2023', 'coverImg25.jpg', 1, 12000),
+    ('26', N'Trường Sinh Từ Thợ Săn Bắt Đầu', '24', 'note26.docx', 'summary26.docx', '20-06-2023', 'coverImg26.jpg', 1, 12000),
+    ('27', N'Đầu Tư Tộc Nhân,Thực Lực Của Ta Là Tộc Tổng Cộng', '24', 'note27.docx', 'summary27.docx', '20-06-2023', 'coverImg27.jpg', 1, 12000),
+    ('28', N'Toàn Dân Thức Tỉnh:Bắt Đầu Thần Thoại Thiên Phú', '24', 'note28.docx', 'summary28.docx', '20-06-2023', 'coverImg28.jpg', 1, 12000),
+    ('29', N'Bán Tiên', '24', 'note29.docx', 'summary29.docx', '20-06-2023', 'coverImg29.jpg', 1, 12000);
