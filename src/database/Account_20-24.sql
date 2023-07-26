8a74073c5fc7036603008591e16e39c49f03c065
Alexive <vuminhquynh23@gmail.com>
2023-07-26 20:35:20 +0700

    Merge all Database files in one file?

--- src/database/Account_20-24.sql
+++
@@ -1,8 +0,0 @@
-SET DATEFORMAT DMY;
-INSERT INTO ACCOUNT (USERID, USERNAME, ACCOUNTNAME, ACCOUNTPASSWORD, BIO, JOBS, FAVORITES, DOB, AVATARURL, COVERURL, ACCOUNTSTATUS, REGDATE, PERMISSION, SDT)
-VALUES
-    ('20', 'thinh1', N'Thịnh Oce', '12345678', 'Bio text1', 'Software Dev', 'Sports, Music', '15-06-2003', 'img20.jpg', 'pcover20.jpg', 0, '01-06-2023', 0, '1234567890'),
-    ('21', 'thinh2', N'Thịnh Too', '12345678', 'Bio text2', 'Designer', 'Movies, Food', '20-10-2003', 'img21.jpg', 'pcover21.jpg', 1, '02-06-2023', 0, '9876543210'),
-    ('22', 'thinh3', N'Thịnh Tree', '12345678', 'Bio text3', 'Marketing', 'Travel, Photography', '12-03-2003', 'img22.jpg', 'pcover22.jpg', 0, '03-06-2023', 0, '4567890123'),
-    ('23', 'thinh4', N'Thịnh For', '12345678', 'Bio text3', 'Marketing', 'Travel, Photography', '13-03-2003', 'img23.jpg', 'pcover23.jpg', 0, '03-06-2023', 1, '4567890124'),
-    ('24', 'thinh5', N'Thịnh Fine', '12345678', 'Bio text3', 'Marketing', 'Travel, Photography', '14-03-2003', 'img24.jpg', 'pcover24.jpg', 0, '03-06-2023', 2, '4567890125');
--- src/database/Notify_20-29.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO NOTIFY (NOTIFYID, TYPEID, CONTENT)
-VALUES
-    ('20', '0', 'noti20.txt'),
-    ('21', '1', 'noti21.txt'),
-    ('22', '3', 'noti22.txt'),
-    ('23', '4', 'noti23.txt');
