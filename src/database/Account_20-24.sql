a3de48734a78618dd32cdd841306af2016a702ca
thinhcu67si <94957063+thinhcu67si@users.noreply.github.com>
2023-07-26 08:08:56 +0700

    Update Account_20-24.sql

--- src/database/Account_20-24.sql
+++ src/database/Account_20-24.sql
@@ -1,7 +1,8 @@
+SET DATEFORMAT DMY;
 INSERT INTO ACCOUNT (USERID, USERNAME, ACCOUNTNAME, ACCOUNTPASSWORD, BIO, JOBS, FAVORITES, DOB, AVATARURL, COVERURL, ACCOUNTSTATUS, REGDATE, PERMISSION, SDT)
 VALUES
-    ('20', 'thinh1', N'Thịnh Oce', '12345678', 'Bio text1', 'Software Dev', 'Sports, Music', '2003-06-15', 'img20.jpg', 'pcover20.jpg', 0, '2023-06-01', 0, '1234567890'),
-    ('21', 'thinh2', N'Thịnh Too', '12345678', 'Bio text2', 'Designer', 'Movies, Food', '2003-10-20', 'img21.jpg', 'pcover21.jpg', 1, '2023-06-02', 0, '9876543210'),
-    ('22', 'thinh3', N'Thịnh Tree', '12345678', 'Bio text3', 'Marketing', 'Travel, Photography', '2003-03-12', 'img22.jpg', 'pcover22.jpg', 0, '2023-06-03', 0, '4567890123'),
-    ('23', 'thinh4', N'Thịnh For', '12345678', 'Bio text3', 'Marketing', 'Travel, Photography', '2003-03-13', 'img23.jpg', 'pcover23.jpg', 0, '2023-06-03', 1, '4567890124'),
-    ('24', 'thinh5', N'Thịnh Fine', '12345678', 'Bio text3', 'Marketing', 'Travel, Photography', '2003-03-14', 'img24.jpg', 'pcover24.jpg', 0, '2023-06-03', 2, '4567890125');
+    ('20', 'thinh1', N'Thịnh Oce', '12345678', 'Bio text1', 'Software Dev', 'Sports, Music', '15-06-2003', 'img20.jpg', 'pcover20.jpg', 0, '01-06-2023', 0, '1234567890'),
+    ('21', 'thinh2', N'Thịnh Too', '12345678', 'Bio text2', 'Designer', 'Movies, Food', '20-10-2003', 'img21.jpg', 'pcover21.jpg', 1, '02-06-2023', 0, '9876543210'),
+    ('22', 'thinh3', N'Thịnh Tree', '12345678', 'Bio text3', 'Marketing', 'Travel, Photography', '12-03-2003', 'img22.jpg', 'pcover22.jpg', 0, '03-06-2023', 0, '4567890123'),
+    ('23', 'thinh4', N'Thịnh For', '12345678', 'Bio text3', 'Marketing', 'Travel, Photography', '13-03-2003', 'img23.jpg', 'pcover23.jpg', 0, '03-06-2023', 1, '4567890124'),
+    ('24', 'thinh5', N'Thịnh Fine', '12345678', 'Bio text3', 'Marketing', 'Travel, Photography', '14-03-2003', 'img24.jpg', 'pcover24.jpg', 0, '03-06-2023', 2, '4567890125');
