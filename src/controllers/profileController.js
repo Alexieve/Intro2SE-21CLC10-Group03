3897d2060101953f4787b614abf8c7d9f862da07
Albert de Nafta <101512551+AlbertNafta@users.noreply.github.com>
2023-08-01 00:47:30 +0700

    Not done yet

---
+++ src/controllers/profileController.js
@@ -0,0 +1,40 @@
+// profileController.js
+
+const Account = require('../models/Account');
+const jwt = require('jsonwebtoken');
+
+const path = require('path');
+
+exports.profilePage = async (req, res) => {
+    try {
+      const token = req.cookies.jwt;
+      if (!token) {
+        throw new Error('No JWT token found');
+      }
+  
+      const decodedToken = jwt.verify(token, 'information of user');
+      const user = await Account.findById(decodedToken.id);
+  
+      if (!user) {
+        throw new Error('User not found');
+      }
+  
+      // Helper function to get the full cover image path
+      const coverImagePath = getCoverImagePath(user.coverURL);
+  
+      // Assuming you have a 'profile' view to render the profile page
+      res.render('profile', { user, coverImagePath });
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Internal Server Error');
+    }
+  };
+
+// Helper function to get the full cover image path
+const getCoverImagePath = (coverURL) => {
+    // Assuming the ProfileCover folder is inside the DataBase directory
+    const databaseDir = path.join(__dirname, '../database');
+    return path.join(databaseDir, 'ProfileCover', coverURL);
+  };
+
+
