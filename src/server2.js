23ad25ce5e4b6dc95b51a9f529c163272e549a53
Albert de Nafta <101512551+AlbertNafta@users.noreply.github.com>
2023-08-01 08:10:29 +0700

    Nếu file này chạy được thì ghi đè file cũ kia luôn

--- src/server2.js
+++ src/server2.js
@@ -8,6 +8,7 @@ const app = express();
 
 // Require Routes
 const authRoutes = require('./routes/authRoutes');
+const profileRoutes = require('./routes/profileRoutes');
 const { requireAuth, checkUser } = require('./middleware/authMiddleware');
 
 // Database connection
@@ -31,6 +32,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(authRoutes);
+app.use(profileRoutes)
 
 // Middleware to check the user for all routes
 app.use(checkUser);
