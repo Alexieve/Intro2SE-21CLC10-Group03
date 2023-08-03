2da7036b65ca66fd27387d4defe0b3480e07b5b0
Albert de Nafta <101512551+AlbertNafta@users.noreply.github.com>
2023-08-03 19:06:35 +0700

    Update BookMark.js

--- src/models/BookMark.js
+++ src/models/BookMark.js
@@ -5,12 +5,10 @@ const bookMarkSchema = new mongoose.Schema({
   bookID: {
     type: Number,
     required: true,
-    unique: true, // If each bookmark should have a unique bookID, keep this. Otherwise, remove it.
   },
   userID: {
     type: Number,
     required: true,
-    unique: true, // If each bookmark should have a unique userID, keep this. Otherwise, remove it.
   },
 });
 
