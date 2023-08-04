6e3681fb5ee458c66440ed3e5cfd0118ac393cb3
Albert de Nafta <101512551+AlbertNafta@users.noreply.github.com>
2023-08-04 19:06:09 +0700

    Add files via upload

--- src/controllers/bookMarkController.js
+++ src/controllers/bookMarkController.js
@@ -10,8 +10,10 @@ const BookMark = require('../models/BookMark');
 const bcrypt = require('bcrypt');
 const fs = require('fs');
 const matchedBooks = [];
-const {bookContainer} = require("../middleware/database");
-const { findLatestVolume,findVolumeName } = require('../public/js/LatestVolume');
+const { findLatestVolume } = require('../public/js/LatestVolume');
+const { bookContainer } = require("../middleware/database");
+const azureBlobBaseUrl = 'https://happinovel.blob.core.windows.net'; // Define the Azure Blob Storage base URL here
+const Chapter = require('../models/Chapter');
 
 
 exports.bookmark = async (req, res) => { // <-- Add 'async' here
@@ -24,9 +26,8 @@ exports.bookmark = async (req, res) => { // <-- Add 'async' here
     const decodedToken = jwt.verify(token, 'information of user');
     const user = await Account.findById(decodedToken.id);
     const BookMId = await BookMark.find({ userID: user.userID });
-const uniqueBookIds = new Set();
-
-// Loop through the bookmarks and find the unique book IDs
+   const uniqueBookIds = new Set();
+    // Loop through the bookmarks and find the unique book IDs
   for (const bookmark of BookMId) {
     uniqueBookIds.add(bookmark.bookID);
   }
@@ -47,11 +48,8 @@ const uniqueBookIds = new Set();
     }
   }
 
-// Now, matchedBooks array contains unique books without duplicates
+  
 
-
-// Now the matchedBooks array contains the books that matched the bookIDs in BookMId
-    
     if (!user) {
       throw new Error('User not found');
     }
@@ -62,9 +60,77 @@ const uniqueBookIds = new Set();
       return await findLatestVolume(book.bookID, bookContainer);
     }));
     const volumes = await Volume.find({ bookID: { $in: bookIDs } });
-    res.render('bookmark', { matchedBooks, bookContainer,latestVolumeUrls,volumes });
+    const chapters = await Chapter.find({
+      $or: matchedBooks.map((book) => ({
+        bookID: book.bookID
+        // Add more conditions if needed
+      })),
+    });
+
+    const latestChapterID = await findLatestChapter(latestVolumeUrls);
+    res.render('bookmark', { matchedBooks, bookContainer,latestVolumeUrls, volumes, latestChapterID, chapters  });
   } catch (err) {
     console.error(err.message);
     res.status(500).send('Internal Server Error');
   }
 };
+
+
+async function findLatestChapter(latestVolumeUrl) {
+  console.log(latestVolumeUrl);
+  try {
+    const urlObject = new URL(latestVolumeUrl);
+    const urlPathname = urlObject.pathname;
+
+    // Extract the volume folder name and bookID from the URL
+    const volumeFolderRegex = /\/Book(\d+)\/Volume(\d+)\//;
+    const match = urlPathname.match(volumeFolderRegex);
+    if (!match) {
+      console.error('Invalid latestVolumeUrl:', latestVolumeUrl);
+      return null;
+    }
+    const bookID = match[1];
+    const volumeNumber = match[2];
+    const volumePath = `Book${bookID}/Volume${volumeNumber}`;
+
+    // Get the list of blobs (chapter files) in the volume's directory
+    const blobs = bookContainer.listBlobsFlat({ prefix: volumePath });
+
+    // Filter blobs based on the chapter file names
+    const chapterFiles = [];
+    for await (const blob of blobs) {
+      chapterFiles.push(blob.name);
+    }
+
+    // Sort the chapter file names in ascending order
+    chapterFiles.sort();
+
+    // Get the latest chapter filename (last one in the sorted array)
+    const latestChapterFileName = chapterFiles[chapterFiles.length - 1];
+
+    // Extract the chapID from the filename
+    const chapIDRegex = /chap(\d+)(?:_(\d+)_(\d+))?\.docx$/i;
+    const chapIDMatch = latestChapterFileName.match(chapIDRegex);
+
+    if (chapIDMatch) {
+      const chapID = chapIDMatch[1];
+      console.log("Latest chapID:", chapID);
+      return chapID;
+    } else {
+      console.error('ChapID not found in the latestChapterFileName:', latestChapterFileName);
+      return null;
+    }
+
+    // Build and return the URL for the latest chapter file
+
+  } catch (err) {
+    console.error('Error finding latest chapter:', err);
+    return null;
+  }
+}
+
+
+
+
+
+
