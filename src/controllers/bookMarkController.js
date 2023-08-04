e66a5e6b13a47026b5b59231c203c4dab2626350
Albert de Nafta <101512551+AlbertNafta@users.noreply.github.com>
2023-08-04 20:21:31 +0700

    Add files via upload

--- src/controllers/bookMarkController.js
+++ src/controllers/bookMarkController.js
@@ -1,80 +1,85 @@
-
 // Assuming you have a 'User' model to fetch user data
-const mongoose = require('mongoose')
-const Account = require('../models/Account');
-const jwt = require('jsonwebtoken');
-const path = require('path');
-const Book = require('../models/Book');
-const Volume = require('../models/Volume');
-const BookMark = require('../models/BookMark');
-const bcrypt = require('bcrypt');
-const fs = require('fs');
+const mongoose = require("mongoose");
+const Account = require("../models/Account");
+const jwt = require("jsonwebtoken");
+const path = require("path");
+const Book = require("../models/Book");
+const Volume = require("../models/Volume");
+const BookMark = require("../models/BookMark");
+const bcrypt = require("bcrypt");
+const fs = require("fs");
 const matchedBooks = [];
-const { findLatestVolume } = require('../public/js/LatestVolume');
+const { findLatestVolume } = require("../public/js/LatestVolume");
 const { bookContainer } = require("../middleware/database");
-const azureBlobBaseUrl = 'https://happinovel.blob.core.windows.net'; // Define the Azure Blob Storage base URL here
-const Chapter = require('../models/Chapter');
+const azureBlobBaseUrl = "https://happinovel.blob.core.windows.net"; // Define the Azure Blob Storage base URL here
+const Chapter = require("../models/Chapter");
 
-
-exports.bookmark = async (req, res) => { // <-- Add 'async' here
+exports.bookmark = async (req, res) => {
+  // <-- Add 'async' here
   try {
     const token = req.cookies.jwt;
     if (!token) {
-      throw new Error('No JWT token found');
+      throw new Error("No JWT token found");
     }
 
-    const decodedToken = jwt.verify(token, 'information of user');
+    const decodedToken = jwt.verify(token, "information of user");
     const user = await Account.findById(decodedToken.id);
     const BookMId = await BookMark.find({ userID: user.userID });
-   const uniqueBookIds = new Set();
+    const uniqueBookIds = new Set();
     // Loop through the bookmarks and find the unique book IDs
-  for (const bookmark of BookMId) {
-    uniqueBookIds.add(bookmark.bookID);
-  }
-
-  const matchedBooks = [];
-
-  // Loop through the unique book IDs and find the matching books
-  for (const bookID of uniqueBookIds) {
-    try {
-      const book = await Book.findOne({ bookID }); // Assuming you have a 'Book' model
-
-      if (book) {
-        // Book with the given bookID found, add it to the array
-        matchedBooks.push(book);
-      }
-    } catch (err) {
-      console.error('Error finding book:', err);
+    for (const bookmark of BookMId) {
+      uniqueBookIds.add(bookmark.bookID);
     }
-  }
 
-  
+    const matchedBooks = [];
+
+    // Loop through the unique book IDs and find the matching books
+    for (const bookID of uniqueBookIds) {
+      try {
+        const book = await Book.findOne({ bookID }); // Assuming you have a 'Book' model
+
+        if (book) {
+          // Book with the given bookID found, add it to the array
+          matchedBooks.push(book);
+        }
+      } catch (err) {
+        console.error("Error finding book:", err);
+      }
+    }
 
     if (!user) {
-      throw new Error('User not found');
+      throw new Error("User not found");
     }
 
     // Helper function to get the full cover image path
-    const bookIDs = matchedBooks.map(book => book.bookID);
-       const latestVolumeUrls = await Promise.all(matchedBooks.map(async (book) => {
-      return await findLatestVolume(book.bookID, bookContainer);
-    }));
+    const bookIDs = matchedBooks.map((book) => book.bookID);
+    const latestVolumeUrls = await Promise.all(
+      matchedBooks.map(async (book) => {
+        return await findLatestVolume(book.bookID, bookContainer);
+      })
+    );
     const volumes = await Volume.find({ bookID: { $in: bookIDs } });
     const chapters = await Chapter.find({
       $or: matchedBooks.map((book) => ({
-        bookID: book.bookID
+        bookID: book.bookID,
         // Add more conditions if needed
       })),
     });
 
     const latestChapterID = await findLatestChapter(latestVolumeUrls);
-    res.render('bookmark', { matchedBooks, bookContainer,latestVolumeUrls, volumes, latestChapterID, chapters  });
+    res.render("bookmark", {
+      matchedBooks,
+      bookContainer,
+      latestVolumeUrls,
+      volumes,
+      latestChapterID,
+      chapters,
+    });
   } catch (err) {
     console.error(err.message);
-    res.status(500).send('Internal Server Error');
+    res.status(500).send("Internal Server Error");
   }
 };
-
 
 async function findLatestChapter(latestVolumeUrl) {
   console.log(latestVolumeUrl);
@@ -86,7 +91,7 @@ async function findLatestChapter(latestVolumeUrl) {
     const volumeFolderRegex = /\/Book(\d+)\/Volume(\d+)\//;
     const match = urlPathname.match(volumeFolderRegex);
     if (!match) {
-      console.error('Invalid latestVolumeUrl:', latestVolumeUrl);
+      console.error("Invalid latestVolumeUrl:", latestVolumeUrl);
       return null;
     }
     const bookID = match[1];
@@ -117,20 +122,16 @@ async function findLatestChapter(latestVolumeUrl) {
       console.log("Latest chapID:", chapID);
       return chapID;
     } else {
-      console.error('ChapID not found in the latestChapterFileName:', latestChapterFileName);
+      console.error(
+        "ChapID not found in the latestChapterFileName:",
+        latestChapterFileName
+      );
       return null;
     }
 
     // Build and return the URL for the latest chapter file
-
   } catch (err) {
-    console.error('Error finding latest chapter:', err);
+    console.error("Error finding latest chapter:", err);
     return null;
   }
 }
-
-
-
-
-
-
