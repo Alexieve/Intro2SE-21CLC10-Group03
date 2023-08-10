ee37d4c2ea6daae69b0432b30258b3520ea2cd15
Albert de Nafta <101512551+AlbertNafta@users.noreply.github.com>
2023-08-10 23:27:58 +0700

    Add files via upload

--- src/routes/profileRoutes.js
+++ src/routes/profileRoutes.js
@@ -7,5 +7,7 @@ const upload = multer();
 
 router.get('/:id', profileController.profilePage);
 router.post('/update-cover', upload.single('coverImage'), profileController.updateCoverImage);
+router.post('/update-avatar', upload.single('avaImage'), profileController.updateAvaImage);
+
 
 module.exports = router;
