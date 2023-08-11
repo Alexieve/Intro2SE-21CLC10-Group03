c8368c1520604b412b679edcd90e62b09266da33
Albert de Nafta <101512551+AlbertNafta@users.noreply.github.com>
2023-08-11 21:37:23 +0700

    Done !

--- src/controllers/profileController.js
+++ src/controllers/profileController.js
@@ -218,31 +218,30 @@ exports.updateAvaImage = async (req, res) => {
 
 
 exports.chang_pass = async (req, res) => {
-  
   try {
-    
     const token = req.cookies.jwt;
     const decodedToken = jwt.verify(token, 'information of user'); // will use later
     const trueUser = await Account.findById(decodedToken.id);
     const oldpass = req.body.inputValue;
     const newpass = req.body.inputValue2;
 
-    const isPasswordValid = await bcrypt.compare(oldpass, trueUser.password);    
-    if(isPasswordValid){
-
-      if(newpass){
-
+    const isPasswordValid = await bcrypt.compare(oldpass, trueUser.password);
+    
+    if (isPasswordValid) {
+      if (newpass) {
         const hashedNewPassword = await bcrypt.hash(newpass, 10);
-
         await Account.findByIdAndUpdate(trueUser._id, { password: hashedNewPassword });
         return res.status(200).send('Password updated successfully.');
       }
+    } else {
+      return res.status(400).send('Invalid old password.');
     }
   } catch (error) {
-    console.error('Error uploading cover image:', error);
+    console.error('Error updating password:', error);
     return res.status(500).send('Internal server error.');
   }
 };
+
 
 exports.bio = async (req, res) => {
   
